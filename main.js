fd0f7a0d196980316e017dd37e6c6779f4029fe6
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-11 13:39:29 -0500

    First API results displaying on page.

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 console.log('javascript connected')
+
 const form = document.querySelector('#form')
 
-//Global Variables
-// let formIsValid
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
-    console.log('Button was clicked')
+    console.log('Button was clicked!')
     const formData = new FormData(event.target)
     const asString = new URLSearchParams(formData).toString('');
     console.log(asString)
-    let url = ('https://itunes.apple.com/seartch?term=jack+johnson&limit=1')
-fetch(url)
-.then(function(response){
-    return response.json
-})
-.then(function(data){
-    console.log(data)
-})
-})
+    let url = (`https://itunes.apple.com/search?${asString}&entity=song&limit=1`)
+    
+    fetch(url)
+        .then(function (response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
 
+            for (let i = 0; i < data.results.length; i++) {
+                console.log('the value of i is:', i)
+                console.log('the artist name is', data.results[i].artistName)
+                document.querySelector('#results-div').innerHTML += `<div>${data.results[i].artistName}</div>`
+            }
+        })
+}) 
 
 // https://itunes.apple.com/seartch?term=jack+johnson&limit=1
